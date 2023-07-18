0ba1506baa6226ad67e3a7f3591d0bc6659a22de
nursyah jaya <nursyahjaya>
2023-07-18 17:08:47 +0700

    tweet layout

---
+++ resources/js/Layouts/Layout.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="min-h-screen flex flex-col pt-6 sm:pt-0">
+        <div class="py-4 mx-auto">
+          <slot />
+        </div>
+    </div>
+</template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,15 @@ export default {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
+            colors: {
+                'blue': '#1DA1F2',
+                'darkblue': '#2795D9',
+                'lightblue': '#EFF9FF',
+                'dark': '#657786',
+                'light': '#AAB8C2',
+                'lighter': '#E1E8ED',
+                'lightest': '#F5F8FA',
+              }
         },
     },
 
